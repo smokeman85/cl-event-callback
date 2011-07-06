12a05fe21749b3005701f731f78e2c6de29cc932
smokeman85 <smokeman85@gmail.com>
2011-07-06 07:40:49 +0000

    Создал пакет, добавил описание к экспортируемым функам

--- event_thread.lisp
+++ event_thread.lisp
@@ -1,5 +1,16 @@
 ;;
 
+(defpackage :cl-event-callback
+  (:use :cl)
+  (:export :add-event
+	   :del-event
+	   :add-callback
+	   :del-callback
+	   :thread-run-callbacks
+	   :thread-stop-callbacks))
+
+(in-package :cl-event-callback)
+
 (defparameter *events* (make-hash-table))
 
 (defparameter *event-state* '(:triggered :not-triggered))
@@ -7,21 +18,27 @@
 (defparameter *callbacks* (make-hash-table))
 
 (defun add-event (name)
+  "add new event"
   (setf (gethash name *events*) (cadr *event-state*)))
 
 (defun del-event (name)
+  "delete event"
   (remhash name *events*))
 
 (defun triggered-event (name)
+  "triggered event"
   (setf (gethash name *events*) (car *event-state*)))
 
 (defun not-triggered-event (name)
+  "dont triggered event"
   (setf (gethash name *events*) (cadr *event-state*)))
 
 (defun add-callback(f name)
+  "Link callback and event"
   (setf (gethash name *callbacks*) f))
 
 (defun del-callback(name)
+  "Delete link"
   (remhash name *callbacks*))
 
 (defun start-callback(name)
@@ -58,9 +75,11 @@
        )))
 
 (defun thread-run-callbacks ()
+  "start handler of events"
   (create-thread '(run-callbacks) "run-callbacks"))
 
 (defun thread-stop-callbacks ()
+  "stop handler of events"
   (if (numberp (find-thread-by-name "run-callbacks"))
       (sb-thread:terminate-thread (nth (find-thread-by-name "run-callbacks") (sb-thread:list-all-threads)))))
 
